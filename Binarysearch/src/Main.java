a2d747fef01c8e925af24238c57c15c2e5818b79
OMRAKESHMITTAL <ommittal0104@gmail.com>
2024-08-23 17:29:58 +0530

    first commit

--- Binarysearch/src/Main.java
+++ Binarysearch/src/Main.java
@@ -38,7 +38,6 @@ public class Main {
         }
         return -1;
     }
-
     public static void main(String[] args) {
         int[] a={1,2,3,4,5,6,7,8,9};
         int[] b={9,8,7,6,5,4,3,2,1};
