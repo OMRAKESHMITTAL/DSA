c2b27ed1182f09532e2cffe04a16d4a2c232c251
OMRAKESHMITTAL <ommittal0104@gmail.com>
2024-08-25 19:03:50 +0530

    first commit

--- Bubble-sort/src/Main.java
+++ Bubble-sort/src/Main.java
@@ -1,7 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
 public class Main {
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void sort(int[] a){
+        int counter = 0;
+        for(int j = 1; j<a.length; j++) {
+            counter = 0;
+        for (int i = 0; i < a.length-j; i++) {
+            if (a[i] > a[i + 1]) {
+                swap(a, i, i + 1);
+                counter = 1;
+            }
+        }
+            if(counter == 0 ){
+                break;
+            }
+    }
+    }
     public static void main(String[] args) {
-        
+        int [] a = {1, 1, 1, 2, 2, 2, 3, 4, 4, 4, 5, 7, 7, 8, 8, 8, 8, 9, 9, 10, 25, 98, 100, 101};
+        sort(a);
+        System.out.println(Arrays.toString(a));
     }
 }
